2430b17da09a89f6da181bed55e232930dbd8980
mathurin42 <josselin.bloy@epitech.eu>
2018-11-04 06:03:23 +0100

    update api

--- modules/chat.js
+++ modules/chat.js
@@ -17,41 +17,55 @@ async function HandleAuthorization(socket, id) {
   socket.emit('error', 'User does not exist');
 }
 
-async function HandleJoinRoom(socket, room, roomId) {
+async function HandleJoinRoom(socket, roomId) {
   const user = User.findOne({ socket: socket.id });
   if (!user) {
     socket.emit('error', 'User not authorized');
-  } else if (!roomId) {
-    const newRoom = new Room();
-    newRoom.name = room;
-    newRoom.users.push(user.id);
-    newRoom.history = [];
-    await newRoom.save();
-
-    user.rooms.push(newRoom);
-    await user.save();
-
-    socket.join(roomId);
-    socket.emit('join-room', newRoom);
-  } else if (roomId) {
-    const roomObj = Room.findOne({ id: roomId });
-    roomObj.users.push(user.id);
-    user.rooms.push(roomObj);
-    await roomObj.save();
-    await user.save();
-
-    socket.join(room);
-    socket.emit('join-room', roomObj);
+    return;
   }
+
+  const roomObj = Room.findOne({ id: roomId });
+  if (!roomObj) {
+    socket.emit('error', 'Room does not exist');
+    return;
+  }
+
+  roomObj.users.push(user.id);
+  user.rooms.push(roomObj);
+  await roomObj.save();
+  await user.save();
+
+  socket.join(roomId);
+  socket.emit('join-room', roomObj);
 }
 
-async function HandleMessage(socket, message, room) {
+async function HandleCreateRoom(socket, name) {
+  const user = User.findOne({ socket: socket.id });
+  if (!user) {
+    socket.emit('error', 'User not authorized');
+    return;
+  }
+
+  const newRoom = new Room();
+  newRoom.name = name;
+  newRoom.users.push(user.id);
+  newRoom.history = [];
+  await newRoom.save();
+
+  user.rooms.push(newRoom);
+  await user.save();
+
+  socket.join(newRoom.id);
+  socket.emit('create-room', newRoom);
+}
+
+async function HandleMessage(io, socket, message, room) {
   const roomObj = Room.findOne({ id: room });
   const user = User.findOne({ socket: socket.id });
   if (!user) {
-    socket.emit('sentMessage', 'User not authorized');
+    socket.emit('error', 'User not authorized');
   } else if (!roomObj) {
-    socket.emit('sentMessage', 'Room does not exist');
+    socket.emit('error', 'Room does not exist');
   } else {
     const newMessage = new Message();
     newMessage.from.push(user);
@@ -61,8 +75,8 @@ async function HandleMessage(socket, message, room) {
 
     roomObj.history.push(newMessage);
     roomObj.save();
-    // TODO: send the conversation history / the room / the message ?
-    socket.in(room).emit('message', newMessage);
+
+    io.in(room).emit('message', roomObj.history);
   }
 }
 
@@ -92,27 +106,55 @@ async function HandleLeaveRoom(socket, roomId) {
   }
 }
 
-async function Handletyping(socket, roomId) {
+async function Handletyping(io, socket, roomId) {
   const roomObj = Room.findOne({ id: roomId });
   const user = User.findOne({ socket: socket.id });
   if (!user) {
-    socket.emit('typing', 'User not authorized');
+    socket.emit('error', 'User not authorized');
   } else if (!roomObj) {
-    socket.emit('typing', 'Room does not exist');
+    socket.emit('error', 'Room does not exist');
   } else {
     const roomIndex = roomObj.users.indexOf(user.id);
     const userIndex = user.rooms.findIndex(element => element.id === roomObj.id);
 
     if (roomIndex === -1) {
-      socket.emit('typing', 'User not in room');
+      socket.emit('error', 'User not in room');
     } else if (!userIndex) {
-      socket.emit('typing', 'User not in room');
+      socket.emit('error', 'User not in room');
     } else {
-      socket.in(roomId).emit('typing', roomObj);
+      io.in(roomId).emit('typing', roomObj);
     }
   }
 }
 
+async function HandleStoptyping(io, socket, roomId) {
+  const roomObj = Room.findOne({ id: roomId });
+  const user = User.findOne({ socket: socket.id });
+  if (!user) {
+    socket.emit('error', 'User not authorized');
+  } else if (!roomObj) {
+    socket.emit('error', 'Room does not exist');
+  } else {
+    const roomIndex = roomObj.users.indexOf(user.id);
+    const userIndex = user.rooms.findIndex(element => element.id === roomObj.id);
+
+    if (roomIndex === -1) {
+      socket.emit('error', 'User not in room');
+    } else if (!userIndex) {
+      socket.emit('error', 'User not in room');
+    } else {
+      io.in(roomId).emit('stop-typing', roomObj);
+    }
+  }
+}
+
+async function HandleDisconnect(socket) {
+  const user = User.findOne({ socket: socket.id });
+  if (user) {
+    user.socket = null;
+    await user.save();
+  }
+}
 /**
  * Chat
  * @param {*} io
@@ -120,20 +162,30 @@ async function Handletyping(socket, roomId) {
 module.exports = {
   async start(io) {
     io.on('connection', socket => {
+      socket.emit('authorization', 'email requested');
       socket.on('authorization', async id => {
         HandleAuthorization(socket, id);
       });
-      socket.on('join-room', (room, id) => {
-        HandleJoinRoom(socket, room, id);
+      socket.on('join-room', id => {
+        HandleJoinRoom(socket, id);
+      });
+      socket.on('create-room', name => {
+        HandleCreateRoom(socket, name);
       });
       socket.on('leave-room', id => {
         HandleLeaveRoom(socket, id);
       });
       socket.on('message', (message, room) => {
-        HandleMessage(socket, message, room);
+        HandleMessage(io, socket, message, room);
       });
       socket.on('typing', room => {
-        Handletyping(socket, room);
+        Handletyping(io, socket, room);
+      });
+      socket.on('stop-typing', room => {
+        HandleStoptyping(io, socket, room);
+      });
+      socket.on('disconnect', () => {
+        HandleDisconnect(socket);
       });
     });
   }
